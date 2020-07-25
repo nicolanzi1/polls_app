ce5a0b46bd133af8b9a55d294bc4f18114a63387
nicolanzi1 <nicolanzi@gmail.com>
2020-07-25 12:46:04 +0100

    Created and migrated the responses table along with the a response model

---
+++ app/models/response.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: responses
+#
+#  id               :bigint           not null, primary key
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  answer_choice_id :integer          not null
+#  respondent_id    :integer          not null
+#
+# Indexes
+#
+#  index_responses_on_answer_choice_id  (answer_choice_id)
+#  index_responses_on_respondent_id     (respondent_id)
+#
+class Response < ApplicationRecord
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_25_111622) do
+ActiveRecord::Schema.define(version: 2020_07_25_112401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 2020_07_25_111622) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["poll_id"], name: "index_questions_on_poll_id"
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.integer "answer_choice_id", null: false
+    t.integer "respondent_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["answer_choice_id"], name: "index_responses_on_answer_choice_id"
+    t.index ["respondent_id"], name: "index_responses_on_respondent_id"
   end
 
   create_table "users", force: :cascade do |t|
