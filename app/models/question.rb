7d9159134bec899532f4f4608886a955d00144e0
nicolanzi1 <nicolanzi@gmail.com>
2020-07-25 12:00:45 +0100

    Created and migrated the questions table along with the question model

---
+++ app/models/question.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :bigint           not null, primary key
+#  text       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  poll_id    :integer          not null
+#
+# Indexes
+#
+#  index_questions_on_poll_id  (poll_id)
+#
+class Question < ApplicationRecord
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_25_105036) do
+ActiveRecord::Schema.define(version: 2020_07_25_105624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2020_07_25_105036) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_polls_on_author_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "text", null: false
+    t.integer "poll_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["poll_id"], name: "index_questions_on_poll_id"
   end
 
   create_table "users", force: :cascade do |t|
