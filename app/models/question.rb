b34840e86282c78d519b9bf118e1088b9cc1165e
nicolanzi1 <nicolanzi@gmail.com>
2020-07-25 15:27:34 +0100

    Wrote 4 results methods in n plus 1, includes queries, SQL queries and ActiveRecord to compare and understand their similarities and differences

--- app/models/question.rb
+++ app/models/question.rb
@@ -28,4 +28,61 @@ class Question < ApplicationRecord
     has_many :responses,
         through: :answer_choices,
         source: :responses
+
+    def results_n_plus_1
+        # Writing the N+1 method
+        results = {}
+        self.answer_choices.each do |ac|
+            results[ac.text] = ac.responses.count
+        end
+        results
+    end
+
+    def resulst_2_queries
+        # Through includes queries where all responses are transferred
+        results = {}
+        self.answer_choices.includes(:responses).each do |ac|
+            results[ac.text] = ac/responses.length
+        end
+        results
+    end
+
+    def results_1_query_SQL
+        # Through SQL queries
+        acs = AnswerChoice.find_by_sql([<<-SQL, id])
+            SELECT
+                answer_choices.*, COUNT(responses.id) AS num_responses
+            FROM
+                answer_choices
+            LEFT OUTER JOIN responses
+                ON answer_choices.id = responses.answer_choices_id
+            WHERE
+                answer_choices.questions_id = ?
+            GROUP BY
+                answer_choices.id
+        SQL
+
+        acs.inject({}) do |results, ac|
+            results[ac.text] = ac.num_responses; results
+        end
+    end
+
+    def results
+        # And with ActiveRecord(best approach, more efficient)
+        acs = self.answer_choices
+            .select("answer_choices.text, COUNT(responses.id) as num_responses")
+            .left_outer_joins(:responses).group("answer_choices.id")
+
+# # If using Rails 4, the method should be written as below, because the is no left_outer_joins method
+#     acs = self.answer_choices
+#         .select("answer_choices.text, COUNT(responses.id), as num_responses")
+#         .joins(<<-SQL).group("answer_choices.id")
+#             LEFT OUTER JOIN responses
+#                 ON answer_choices.id = responses.answer_choices_id
+#         SQL
+
+        acs.inject({}) do |results, ac|
+            results[ac.text] = ac.num_responses; results
+        end
+    end
 end
